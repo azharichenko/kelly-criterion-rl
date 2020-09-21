d1210a933435b3391e30aca4d38ba4242df748fa
Alex D. Zharichenko <azharichenko@gmail.com>
2020-09-21 00:01:57 -0400

    Adding in experiement with training through ray

--- kelly_gym/__init__.py
+++ kelly_gym/__init__.py
@@ -8,6 +8,4 @@ register(
     id='Kelly-v0',
     entry_point='kelly_gym.envs:KellyEnv',
     max_episode_steps =100,
-    # reward_threshold=1.0,
-    # nondeterministic = True,
 )
--- kelly_gym/envs/kelly_env.py
+++ kelly_gym/envs/kelly_env.py
@@ -1,15 +1,17 @@
 import gym
 from gym import spaces
 from gym import utils
-from gym.utils import seeding
+from gym.utils import seeding, EzPickle
 import numpy as np
 
 from random import randint
 
+import plotly.graph_objects as go
+
 import logging
 logger = logging.getLogger(__name__)
 
-class KellyEnv(gym.Env):
+class KellyEnv(gym.Env, EzPickle):
   metadata = {'render.modes': ['human']}
 
   def __init__(self, starting_capital: float=25.0, prob: float=0.6):
@@ -19,16 +21,19 @@ class KellyEnv(gym.Env):
     ])
     self.observation_space = spaces.Box(
             low=np.array([0.0, 0.0]),
-            high=np.array([255.0, 1.0]),
+            high=np.array([np.Inf, 1.0]),
             dtype=np.float32
     )
 
-    self.starting_capital = starting_capital
+    self.starting_capital = 25.0
     self.capital = starting_capital
     self.heads_probability = prob
 
+    self.history = [self.starting_capital]
+
   def reset(self):
     self.capital = self.starting_capital
+    self.history = [self.starting_capital]
     return [self.capital, self.heads_probability]
 
   def step(self, action):
@@ -42,17 +47,20 @@ class KellyEnv(gym.Env):
       reward = 1
     else:
       self.capital -= bet
-      reward = -1
+      reward = -2
 
     self.capital = round(self.capital, 2)
+    self.history.append(self.capital)
 
     if  self.capital <= 0:
       done = True
 
     return [self.capital, self.heads_probability], reward, done, {}
 
-#   def render(self, mode='human'):
-#     ...
+  def render(self, mode='human'):
+    indexes = np.arange(len(self.history))
+    fig = go.Figure(data=go.Scatter(x=indexes, y=self.history))
+    fig.show()
 
 #   def close(self):
 #     ...
